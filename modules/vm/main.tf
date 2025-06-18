2c1a3ea97875774608d36676aadfe54f3c24b111
kkmachani <118263597+kkmachani@users.noreply.github.com>
2025-06-18 10:24:58 +0530

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = var.ip_name
-    subnet_id                     = var.subnet.id
+    subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
   }

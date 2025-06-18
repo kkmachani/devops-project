a528aff56f56c4fa2f9bdbc9e67547170f114b72
kkmachani <118263597+kkmachani@users.noreply.github.com>
2025-06-18 11:39:13 +0530

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,18 +1,18 @@
-variable "subscription_id" {
-    type = string
-}
+# variable "subscription_id" {
+#    type = string
+# }
 
-variable "tenant_id" {
-    type = string
-}
+# variable "tenant_id" {
+#     type = string
+# }
 
-variable "client_id" {
-    type = string
-}
+# variable "client_id" {
+#     type = string
+# }
 
-variable "client_secret" {
-    type = string
-}
+# variable "client_secret" {
+#     type = string
+# }
 
 variable "resource_group" {
   type = string

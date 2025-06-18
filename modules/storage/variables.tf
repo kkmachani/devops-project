9dac9e38768ef4fbf7553c3342f0637f354df697
kkmachani <118263597+kkmachani@users.noreply.github.com>
2025-06-18 13:32:26 +0530

    Create variables.tf

---
+++ modules/storage/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group" {
+    type = string
+}
+
+variable "sa_name" {
+    type = string
+}
+
+variable "account_tier" {
+    type = string
+}
+
+variable "sa_tag" {
+    type = string
+}
+
+variable "sa_container" {
+    type = string
+}

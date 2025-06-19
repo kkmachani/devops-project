954127f1bb74c824f38b8814c20e495f09c5ef9d
kkmachani <118263597+kkmachani@users.noreply.github.com>
2025-06-19 16:36:43 +0530

    Update outputs.tf

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,24 +1,30 @@
 # Vnet Output Variables
 output "vnet_name" {
- value = module.vnet.vnet_name
+    value = module.vnet.vnet_name
 }
 
-output "subnet_name" {
- value = module.vnet.subnet_name
+output "subnet_names" {
+    value = module.vnet.subnet_names
 }
 
+output "subnet_ids" {
+    value = module.vnet.subnet_ids
+}
+
+# VM Output Variables
 output "vm_name" {
- value = module.vm.vm_name
+    value = module.vm.vm_name
 }
 
 output "vm_size" {
- value = module.vm.vm_size
+    value = module.vm.vm_size
 }
 
-output "subnet_id" {
- value = module.vnet.subnet_id
+output "vm_subnet_id" {
+    value = module.vnet.vm_subnet_id
  }
 
+# NSG Output Variables
 output "nsg_name" {
     value = module.nsg.nsg_name
 }
@@ -27,14 +33,11 @@ output "nsg_id" {
     value = module.nsg.nsg_id
 }
 
-# output "aks_name" {
-#  value = module.aks.aks_name
-# }
+# Public IP
+output "ip_names" {
+  value = module.public_ip.ip_names
+}
 
-# output "dns_prefix" {
-#  value = module.aks.dns_prefix
-# }
-
-# output "aks_nodepool" {
-#  value = module.aks.aks_nodepool
-# }
+output "ip_ids" { 
+  value = module.public_ip.ip_ids
+}

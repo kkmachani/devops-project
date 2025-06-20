2d2339308659ceb3c7826faafff5eafb132e0b4f
kkmachani <118263597+kkmachani@users.noreply.github.com>
2025-06-20 15:03:51 +0530

    Update variables.tf

--- environments/qa/variables.tf
+++ environments/qa/variables.tf
@@ -146,3 +146,45 @@ variable "ip_names" {
 variable "ip_tag" {
     type = string
 }
+
+# Application Gwatway Variables
+
+variable "agw_name" {
+  type = string
+}
+
+variable "ag_subnet_id" {
+  type = string
+}
+
+variable "gateway_ip_configuration" {
+  type = string
+}
+
+variable "frontend_port" {
+   type = string
+}
+
+variable "frontend_ip_configuration" {
+   type = string
+}
+
+variable "agw_ip_id" {
+  type = string
+}
+
+variable "backend_address_pool" {
+  type = string
+}
+
+variable "backend_http_settings" {
+  type = string
+}
+
+variable "http_listener" {
+  type = string
+}
+
+variable "request_routing_rule" {
+   type = string
+}

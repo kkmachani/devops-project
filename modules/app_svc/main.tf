61442a3538c7e3a487163eee63d387053a016e6b
kkmachani <118263597+kkmachani@users.noreply.github.com>
2025-06-23 13:25:34 +0530

    Create main.tf

---
+++ modules/app_svc/main.tf
@@ -0,0 +1,28 @@
+data "azurerm_resource_group" "existing_rg" {
+  name = var.resource_group
+}
+
+resource "azurerm_service_plan" "asp" {
+  name = var.asp
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  location = data.azurerm_resource_group.existing_rg.location
+  sku_name = "B1"
+  os_type = "Linux"
+}
+
+resource "azurerm_linux_web_app" "aps" {
+  name = var.aps
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  location            = data.azurerm_resource_group.existing_rg.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config{
+    application_stack {
+      php_version = "8.0"
+    }
+  }
+
+   tags = {
+      Env = "Dev"
+    }
+}

cb464249a52a63d16db7b08748424dd2f577cd1e
kkmachani <118263597+kkmachani@users.noreply.github.com>
2025-06-30 11:11:47 +0530

    Update outputs.tf

--- modules/public_ip/outputs.tf
+++ modules/public_ip/outputs.tf
@@ -7,3 +7,7 @@ output "ip_ids" {
   description = "List of all created public ip ids"
   value       = azurerm_public_ip.public_ip[*].id
 }
+
+output "agw_ip_id" {
+   value = azurerm_public_ip.public_ip[0].id
+}
